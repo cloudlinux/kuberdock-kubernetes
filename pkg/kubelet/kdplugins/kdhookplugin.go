31f455421edb0f11c9f5672093f9905007b3fc0c
Aborilov Pavel <paborilov@cloudlinux.com>
2016-08-04 14:03:52 +0300

    test commit
    Change-Id: I75dbde6f92892c87e6c44e0194bb07c74daaa492

--- pkg/kubelet/kdplugins/kdhookplugin.go
+++ pkg/kubelet/kdplugins/kdhookplugin.go
@@ -1,6 +1,8 @@
 package kdplugins
 
 import (
+	"time"
+
 	"github.com/golang/glog"
 	"k8s.io/kubernetes/pkg/api"
 )
@@ -9,15 +11,21 @@ type KDHookPlugin struct {
 }
 
 func (p *KDHookPlugin) OnContainerCreatedInPod(container *api.Container, pod *api.Pod) {
-	glog.V(4).Infof(">>>>>>>>>>> Container %q created in pod %q", container.Name, pod.Name)
+	glog.V(4).Infof(">>>>>>>>>>> Container %q created in pod! %q", container.Name, pod.Name)
+	time.Sleep(5000 * time.Millisecond)
+	glog.V(4).Infof(">>>>>>>>>>> End Container %q created in pod! %q", container.Name, pod.Name)
 }
 
 func (p *KDHookPlugin) OnPodRun(pod *api.Pod) {
-	glog.V(4).Infof(">>>>>>>>>>> Pod %q run", pod.Name)
+	glog.V(4).Infof(">>>>>>>>>>> Pod %q run!", pod.Name)
+	time.Sleep(5000 * time.Millisecond)
+	glog.V(4).Infof(">>>>>>>>>>> End Pod %q run!", pod.Name)
 }
 
 func (p *KDHookPlugin) OnPodKilled(pod *api.Pod) {
 	if pod != nil {
 		glog.V(4).Infof(">>>>>>>>>>> Pod %q killed", pod.Name)
+		time.Sleep(5000 * time.Millisecond)
+		glog.V(4).Infof(">>>>>>>>>>> End Pod %q killed", pod.Name)
 	}
 }
